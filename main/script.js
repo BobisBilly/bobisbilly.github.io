38b46137ceb5b835e27d023a948137cbd125fd23
BobisBilly <devindadudenz@gmail.com>
2024-05-01 18:18:28 +1200

    modified:   index.html 	modified:   main/script.js 	modified:   main/style.css

---
+++ main/script.js
@@ -0,0 +1,12 @@
+var enabled = false;
+
+function incorrect() {
+    if (enabled != true) {
+        document.getElementById("title-text").innerHTML = 'This sites pourpose is to view <a href="https://discord.com/users/1022602321370296371" target="_blank" class="user">@BobisBilly</a>\'s current projects.';
+        enabled = true;
+    } else {
+        document.getElementById("title-text").innerHTML = 'This sites purpose is to view <a href="https://discord.com/users/1022602321370296371" target="_blank" class="user">@BobisBilly</a>\'s current projects.';
+        enabled = false;
+    }
+}
+    
